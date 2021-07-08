2e50ce18d1f2ae01df4f512eeb48ff7f5bbc4230
Deep Patel <dmpate@deakin.edu.au>
2021-07-08 15:28:41 +1000

    Implemented Dice Roller logic into MainActivity.kt - Created Class Dice - Created rollDice function

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +9,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Fetch Button View using Id into rollButton
         val rollButton: Button = findViewById(R.id.button2)
+        // Create onClickListener to assign activity for the clicked button
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        TODO("Not yet implemented")
+    }
+
+    // Dice class to implement Rolling Dice logic
+    class Dice(val numSides:Int){
+        fun roll(): Int {
+            return (1..numSides).random()
         }
     }
 }
